e9c52c6ee99a3b6e73322ea43cd797d0fda3d203
lenis03 <fardintorkamand83@gmail.com>
2023-03-20 11:57:37 +0330

    add media config to project

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 urlpatterns = [
@@ -22,4 +24,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('', include('books.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
